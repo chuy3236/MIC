98df4351ad09db432ebb7ddeec0089424595f5a8
JDragon <10100aaaaa@gmail.com>
2014-04-17 22:27:20 +0900

    asdfasd
    asdfasdfasdf

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  11
+//
+//  Created by 정주용 on 2014. 4. 17..
+//  Copyright (c) 2014년 정주용. All rights reserved.
+//
+
+#include <stdio.h>
+#include <time.h>
+int main(int argc, const char * argv[])
+{
+
+    printf("asdfasdf\n");
+    
+    return 0;
+}
+
